9ffdffaf3d812b03bd9cff6d53128e5a8ee22edf
blagoySimandov <baggy.simandoff@gmail.com>
2025-03-23 15:02:37 +0000

    fix: corrected the print pattern

--- print-pattern/Main.java
+++ print-pattern/Main.java
@@ -1,26 +1,17 @@
-import java.util.Arrays;
-
 class Main {
-  public static void p(Object... args) {
-    Arrays.stream(args).forEach(System.out::println);
-  }
-
   public static void main(String[] args) {
     final char printChar = '*';
     final int rows = 10;
-    for (int i = 1; i <= rows; i++) {
-      // Print spaces - using two spaces for each position
-      for (int j = 1; j <= (rows - i) * 2; j++) {
-        System.out.print(" ");
+    for (int i = 0; i < rows; i++) {
+      int spaces = rows - i;
+      String stringToBePrinted = "";
+      for (int k = 0; k < spaces; k++) {
+        stringToBePrinted += " ";
       }
-
-      // Print stars
-      for (int k = 1; k <= i; k++) {
-        System.out.print("*");
+      for (int j = 0; j <= i; j++) {
+        stringToBePrinted += printChar;
       }
-
-      // Move to the next line
-      System.out.println();
+      System.out.println(stringToBePrinted);
     }
   }
 }
