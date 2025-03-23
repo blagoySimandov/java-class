511ae63133ddf3af4ccee734bc376354c48034a6
blagoySimandov <baggy.simandoff@gmail.com>
2025-03-23 15:04:53 +0000

    chore: rename for consistency

--- print-pattern-steps/Main.java
+++ print-pattern-steps/Main.java
@@ -1,7 +1,8 @@
 class Main {
   public static void main(String[] args) {
     final char printChar = '*';
-    final int rows = 10;
+    final int rows = 5;
+    System.out.printf("Printing pattern steps patter with: %d rows \n", rows);
     for (int i = 0; i < rows; i++) {
       int spaces = rows - i;
       String stringToBePrinted = "";
